6d17fa698d56304ff490378427ea2460a15cde78
Nishant-Universe <137395428+Nishant-Universe@users.noreply.github.com>
2025-03-16 07:38:12 +0000

    a

---
+++ 05_iterations/six.js
@@ -0,0 +1,14 @@
+//for each   loop
+
+const coading=["js","ruby","py","cpp"]
+
+
+// const values=coading.forEach((item)=>{
+//    // console.log(item);
+//     return(item);
+// })
+
+// console.log(values);
+
+const nums=[1,2,3,4,5,6,7,8,9,10]
+myn
